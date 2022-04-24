e343a4ad59d04f47d3c2651e17b692a48192c0c8
LankyCyril <cyril@ro.ru>
2022-04-24 00:13:39 -0400

    kahlua.keffil.EffilThreadPool

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 return {
     sys = require "kahlua.sys"; -- sys.lua
     structures = require "kahlua.structures"; -- structures.lua
+    keffil = require "kahlua.keffil"; -- keffil.lua
     gzip = require "kahlua.gzip"; -- gzip.lua
     itertools = require "kahlua.itertools";  -- itertools.lua
 }
