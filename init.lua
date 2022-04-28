e644570100e02d7dbec1d46ba83f26a0a6a54ae0
LankyCyril <cyril@ro.ru>
2022-04-27 21:23:20 -0400

    NB! REMOVE itertools (too slow; might reconsider in the future)

--- init.lua
+++ init.lua
@@ -2,5 +2,4 @@ return {
     sys = require "kahlua.sys"; -- sys.lua
     structures = require "kahlua.structures"; -- structures.lua
     gzip = require "kahlua.gzip"; -- gzip.lua
-    itertools = require "kahlua.itertools";  -- itertools.lua
 }
