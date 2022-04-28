8de3123ef2a3f2d18893b04e24223b94485000d0
LankyCyril <cyril@ro.ru>
2022-04-28 01:49:28 -0400

    Itertools again

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 return {
     sys = require "kahlua.sys"; -- <sys.lua>
+    itertools = require "kahlua.itertools"; -- <itertools.lua>
     structures = require "kahlua.structures"; -- <structures.lua>
     shm = require "kahlua.shm"; -- <shm.lua>
     parallel = require "kahlua.parallel"; -- <parallel.lua>
