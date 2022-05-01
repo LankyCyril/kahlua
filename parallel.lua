da3f961dcb60c6291f35eb1f3cf916c21784620d
LankyCyril <cyril@ro.ru>
2022-04-30 23:07:51 -0400

    New implementation of LoopingShmemThread (actually tested)

--- parallel.lua
+++ parallel.lua
@@ -6,112 +6,86 @@ local shm = require "kahlua.shm" -- <shm.lua>
 
 
 parallel.LoopingShmemThread = function (o)
-    -- Wrapper around an `effil.thread()` that loops `loop(cdata)` and passes cdata in an out of `loop` via `Shmem` --
- 
-    local cdef, intype, outtype = o.cdef, o.intype, o.outtype
-    local intype_cast, outtype_cast = o.intype_cast, o.outtype_cast
-    local init, loop = o.init or function () return {} end, o.loop
-    if (not intype) or (not outtype) then
-        error("LoopingShmemThread: intype and outtype must be defined")
-    elseif cdef then
-        pcall(function () ffi.cdef(cdef) end)
-    end
-    local memsize = math.max(ffi.sizeof(intype), ffi.sizeof(outtype))
-    local shmem = shm.Shmem(memsize)
-    local in_channel, out_channel = effil.channel(), effil.channel()
-    local thread = {
-        id=shmem.id, polling=false, state=effil.table{status="idle", err=nil},
+    -- Loop function `func(cdata, ping, pong)` that resumes on ping, modifies cdata in shared memory in-place, and pauses with pong --
+    local shmem = shm.Shmem(ffi.sizeof(o.ctype))
+    local shmem_id = shmem.id
+    local ping, pong, lock = effil.channel(), effil.channel(), effil.channel()
+    return {
+        ping = ping; pong = pong; lock = lock;
+        __shmem = shmem; -- need to keep reference, will get GC'd otherwise
+        cdata = shmem:cast(o.ctype_cast or o.ctype);
+        thread = effil.thread(function (...)
+            local ffi = require "ffi" -- <backend/luajit/src/lib_ffi.c>
+            ffi.cdef(o.cdefs or "")
+            local shm = require "kahlua.shm" -- <backend/lualibs/kahlua/shm.lua>
+            local shmem = shm.Shmem(ffi.sizeof(o.ctype), shmem_id, "unlink")
+            local cdata = shmem:cast(o.ctype_cast or o.ctype)
+            o[1](cdata, ping, pong)
+        end)();
+        release = function (self)
+            while self.lock:size() > 0 do
+                self.lock:pop()
+            end
+        end;
+        _is_completed = function (self)
+            local status, e = self.thread:status()
+            return (status == "failed") and error(e) or (status == "completed")
+        end;
     }
+end
+
+
+parallel.LoopingShmemThreadPool = function (options)
+    -- Docstring goes here lol --
  
-    thread.runner = effil.thread(function ()
-        -- Run `loop` for each incoming piece of cdata, notified by non-`nil` ping on `in_channel`; finish when next ping is `nil` --
-        _ = cdef and require("ffi").cdef(cdef)
-        local _shmem = require("kahlua.shm").Shmem(memsize, thread.id, "unlink")
-        local cdata, luadata, ping, success = nil, nil, nil, true
-        local fenv = getfenv(); for k, v in pairs(init()) do
-            fenv[k] = fenv[k] and error("Cannot redefine " .. k) or v
+    ffi.cdef(options.cdefs or "")
+    local ERROR_FORBID_ADD = "Cannot add threads to an already active pool"
+ 
+    local _yield_or_exhaust = function (self, action, previous_thread, timeout_ms)
+        if previous_thread then
+            previous_thread.lock:push(true)
+            previous_thread.ping:push(true)
+        elseif action == "yield" then
+            self.add = function () error(ERROR_FORBID_ADD) end
+            return self.threads[1], self.threads[1].cdata
+        end
+        if not timeout_ms then
+            for _, thread in ipairs(self.threads) do
+                thread.ping:push(nil)
+                thread.thread:wait()
+            end
         end
         while true do
-            luadata, ping = in_channel:pop()
-            if ping == nil then
-                break
-            elseif intype_cast then
-                thread.state.status = "casting_in"
-                cdata = _shmem:cast(intype_cast)
-            else
-                thread.state.status = "importing"
-                cdata = _shmem:read(intype)
-            end
-            thread.state.status = "running"
-            success, cdata, luadata = pcall(function ()
-                return loop(cdata, luadata)
-            end)
-            if not success then
-                thread.state.status, thread.state.err = "failed", cdata
-                break
-            end
-            if cdata ~= nil then -- not modified in place
-                _shmem:write(cdata, outtype)
-            end
-            thread.state.status = "presenting"
-            out_channel:push(luadata, true)
-        end
-        thread.state.status = "stopped"
-        out_channel:push(nil, nil)
-    end)()
- 
-    thread.join = function (self)
-        -- Wait for thread to finish queued loops, then join and stop; does not collect results of function --
-        _ = thread.state.err and error(thread.state.err)
-        in_channel:push(nil, nil)
-        thread.state.status = "joining"
-        thread.runner:wait()
-        thread.state.status = "stopped"
-        _ = thread.state.err and error(thread.state.err)
-    end
- 
-    thread.write = function (self, cdata, luadata)
-        -- Non-blocking write that is allowed to fail if thread is busy (not "idle"); returns `true` on success, `nil` otherwise --
-        if thread.state.status == "idle" then
-            shmem:write(cdata, intype)
-            in_channel:push(luadata, true)
-            thread.state.status = "data_queued"
-            return true
-        end
-    end
- 
-    thread.read = function (self, timeout_ms, last)
-        -- Read that blocks for `timeout_ms` ms, or indefinitely if `timeout_ms` is `nil`; returns function result or `nil` --
-        if last then
-            thread:join()
-        end
-        if not thread.polling then -- XXX TODO nasty race conditions if more than one poller
-            thread.polling = true
-            local cdata, luadata, ping = nil, nil, nil
-            if (timeout_ms == nil) or (thread.state.status == "presenting") then
-                luadata, ping = out_channel:pop(timeout_ms, "ms")
-            end
-            _ = thread.state.err and error(thread.state.err)
-            if ping then
-                if outtype_cast then
-                    thread.state.status = "casting_out"
-                    cdata = shmem:cast(outtype_cast)
-                else
-                    thread.state.status = "exporting"
-                    cdata = shmem:read(outtype)
+            local n_completed = 0;
+            for _, thread in ipairs(self.threads) do
+                if thread:_is_completed() then
+                    if thread.pong:size() > 0 then
+                        coroutine.yield(thread, thread.cdata)
+                    end
+                    n_completed = n_completed + 1
+                elseif thread.pong:pop(timeout_ms) then
+                    coroutine.yield(thread, thread.cdata)
+                elseif timeout_ms and (thread.lock:size() == 0) then
+                    return thread, thread.cdata
                 end
-                thread.state.status = "idle"
             end
-            thread.polling = false
-            return cdata, luadata
+            if n_completed == self.n_threads then
+                break
+            end
         end
     end
  
-    thread.status = function (self)
-        return thread.state.status, thread.state.err
-    end
- 
-    return thread
+    return {
+        cdefs = options.cdefs; n_threads = 0; threads = {};
+        add = function (self, o)
+            self.n_threads = self.n_threads + 1
+            self.threads[self.n_threads] = parallel.LoopingShmemThread{
+                cdefs=self.cdefs, ctype=o.ctype, ctype_cast=o.ctype_cast, o[1],
+            }
+        end;
+        yield = function (s, ...) return _yield_or_exhaust(s, "yield", ...) end;
+        exhaust = function (s) return _yield_or_exhaust(s, "last") end;
+    }
 end
 
 
