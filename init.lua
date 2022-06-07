c817ac3eaf6f969e8e9161021218299134062437
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-07 09:12:06 +0800

    Delete init.lua

--- init.lua
+++
@@ -1,6 +0,0 @@
-return {
-    sys = require "kahlua.sys"; --[[sys.lua]]
-    gzip = require "kahlua.gzip"; --[[gzip.lua]]
-    shm = require "kahlua.shm"; --[[shm.lua]]
-    parallel = require "kahlua.parallel"; --[[parallel.lua]]
-}
